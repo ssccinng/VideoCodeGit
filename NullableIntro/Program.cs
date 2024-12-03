163ad923a5cc68060d2cd89763394c922737eb5f
scixing <whs11998@163.com>
2024-12-03 13:30:00 +0800

    NRT update

---
+++ NullableIntro/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-tesmplate for more information
+
+string a = null;
+
+string? c = String.Empty;
+
+c = a;
+Test("");
+Test(c);
+Test(a);
+Test(null);
+Test(null!);
+
+return;
+
+void Test(string b)
+{
+    Console.WriteLine(b);
+}
+
+void Test1(string? b)
+{
+    
+    Console.WriteLine(b.Length);
+    b ??= string.Empty;
+    Console.WriteLine(b.Length);
+}
+
